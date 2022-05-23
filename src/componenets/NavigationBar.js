6649f428a9c7188d7356e8d113dcab86c7e1bf85
Omar <olr7742@nyu.edu>
2022-05-23 11:53:24 +0400

    Initial Commit

---
+++ src/componenets/NavigationBar.js
@@ -0,0 +1,9 @@
+import logo from "../logo" 
+
+export default function NavigationBar(){
+    return (
+        <div className = "NavigationBar">
+            <img src={logo}></img>
+        </div>
+    )
+}
